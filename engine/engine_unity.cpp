f671acfdda79519aab0cff60c63cb3e309534742
Dustin Hollar <dustinrhollar@gmail.com>
2020-05-04 21:48:42 -0400

    adjusted unity files to better support a game layer

--- engine/engine_unity.cpp
+++ engine/engine_unity.cpp
@@ -1,9 +1,46 @@
 
+
+//~ Required Game level functions - must be declared here
+// These are functions the engine needs to about related to the game.
+// For example, once the engine is done setting up internal behaviors,
+// it calls into "GameInit()" and when it is ready to render the next
+// frame "GameUpdateAndRender"
+
+void GameInit();
+void GameShutdown();
+void GameUpdateAndRender();
+void FlagGameResize();
+
+
 //~ Vulkan includes
 #include <vulkan/vulkan.h>
-//#include <GLFW/glfw3.h>
 
-// Unity build file for all engine code
+//~ Required C++ Headers
+//~ CLib  Headers
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+// NOTE(Dustin): Where is this actually used?
+#include <stddef.h>
+// Used for determining if a registered component
+// inherits from IComponent
+#include <type_traits>
+// needed for placement new
+#include <new>
+// NOTE(Dustin): Maybe can replace with hashtable
+#include <set>
+// NOTE(Dustin): Maybe can replace with jstring
+#include <string>
+#include <new>
+// NOTE(Dustin): I don't think this header is actually needed
+#include <iostream>
+// NOTE(Dustin): Handle this better: Used for VK_CHECK_RESULT
+#include <stdexcept>
+// NOTE(Dustin): Is this still necessary?
+#include <optional>
 
 //~ Type System and Configuration
 #include "utils/maple_types.h"
